a2df04a0e755c4f0e59b43cdc9f9c4d94d81a4c1
Stefania De Flaviis <stefania.deflaviis@gmail.com>
2022-10-26 16:45:24 +0200

    add function slider
    function slider is called on button click. According to the direction of
    button, slider goes to the next or prev image

--- js/script.js
+++ js/script.js
@@ -21,22 +21,22 @@ const sliderItems = [
   },
   {
     title: "Perù",
-    description: "Stoccolma, la capitale, è una città moderna, anche se vi sono zone che conservano un'atmosfera paesana.",
+    description: "Il Perù è un paese pieno di ricchezze. La natura selvaggia si mischia a una cultura millenaria, fatta di storie affascinanti e di leggende che si perdono indietro nel tempo",
     image: "https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg"
   },
   {
     title: "Cile",
-    description: "Stoccolma, la capitale, è una città moderna, anche se vi sono zone che conservano un'atmosfera paesana.",
+    description: "Tanti paesi in uno: ecco l’essenza del Cile. Tanto lungo geograficamente quanto multiforme per clima, paesaggi ed ecosistemi. Gli altopiani aridi del deserto di Atacama sono diversissimi dalle vigne colorate della Valle di Casablanca.",
     image: "https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c"
   },
   {
     title: "Argentina",
-    description: "Stoccolma, la capitale, è una città moderna, anche se vi sono zone che conservano un'atmosfera paesana.",
+    description: "L'incredibile varietà della sua geografia e la sua vivace cultura, un mix di radici indigene e influenze europee, danno vita a un paese ricchissimo di spunti, contrasti e grandi passioni",
     image: "https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg"
   },
   {
     title: "Colombia",
-    description: "Stoccolma, la capitale, è una città moderna, anche se vi sono zone che conservano un'atmosfera paesana.",
+    description: "Dalle cordigliere al deserto, dal Mar dei Caraibi fino all'Amazzonia, la Colombia è una destinazione eccitante che offre qualcosa per tutti i gusti",
     image: "https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop"
   },
 ]
@@ -45,7 +45,7 @@ const sliderItems = [
 
 Script
 1 Aggiungere evento al click bottone:
-  Chiama funzione removeImage corrente
+  
   Rimuove classe active dalla thumbnails corrente, prende stesso argomento delle funzioni remove
   Chiama funzione printImage successiva
   Aggiunge classe active alla thumbnails successiva, prende stesso argomento delle funzioni print
@@ -54,10 +54,20 @@ Script
   Su quell'oggetto e prende le informazioni
 */
 
-printImage(sliderItems[0]);
+const wrapImage = document.querySelector(".wrap-slider");
+const btnNext = document.querySelector(".btn-slide.next");
+const btnPrev = document.querySelector(".btn-slide.prev");
+let countIndex = 0;
+
+btnNext.addEventListener("click", function() {
+  slider("next");
+});
+
+btnPrev.addEventListener("click", function() {
+  slider("prev");
+});
 
 function printImage(item) {
-  const wrapImage = document.querySelector(".wrap-slider");
   const output = `
   <img src="${item.image}" alt="${item.title}">
   <div class="reference">
@@ -68,4 +78,20 @@ function printImage(item) {
   </div>
   `;
   wrapImage.innerHTML = output;
+}
+
+printImage(sliderItems[countIndex]);
+function slider(direction) {
+  if (direction === "next") {
+    countIndex++;
+    if (countIndex >= sliderItems.length) {
+      countIndex = 0;
+    }
+  } else if (direction === "prev") {
+    countIndex--;
+    if (countIndex < 0) {
+      countIndex = (sliderItems.length - 1);
+    }
+  }
+  printImage(sliderItems[countIndex]);
 }
